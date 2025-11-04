5c6939927279d4905903aeebfc18039cb997ccb0
geoffreymamcer <115859560+geoffreymamcer@users.noreply.github.com>
2025-11-04 20:39:22 +0800

    fixed the patient demographics chart on admin dashboard

--- frontend/src/AdminSide/PatientList/PatientDemographicsChart.jsx
+++ frontend/src/AdminSide/PatientList/PatientDemographicsChart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import instance from "../../api/axios";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
@@ -14,10 +14,8 @@ const PatientDemographicsChart = () => {
   useEffect(() => {
     const fetchAgeData = async () => {
       try {
-        const token = localStorage.getItem("token");
-        const response = await axios.get(
-          "http://localhost:5000/api/analytics/age-group-distribution",
-          { headers: { Authorization: `Bearer ${token}` } }
+        const response = await instance.get(
+          "/analytics/age-group-distribution"
         );
 
         const ageGroups = response.data;
